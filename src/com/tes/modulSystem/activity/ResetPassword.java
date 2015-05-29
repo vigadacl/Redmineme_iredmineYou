5c44c04e3aa565458a09966cb7d0616ed4845dda
Steeve <nkemson@tzi.de>
2015-05-29 13:07:56 +0200

    this class help to have a new password, when we have forget the old password

---
+++ src/com/tes/modulSystem/activity/ResetPassword.java
@@ -0,0 +1,17 @@
+package com.tes.modulSystem.activity;
+
+import com.tes.modulSystem.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResetPassword extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.resetpass);
+	}
+
+}
